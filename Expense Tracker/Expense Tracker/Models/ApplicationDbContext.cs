daad854bb88d9855a2eac823bf7950e21685ddc3
Victor <victorphiathep@gmail.com>
2023-09-17 18:13:17 -0400

    25:00 mark

--- Expense Tracker/Expense Tracker/Models/ApplicationDbContext.cs
+++ Expense Tracker/Expense Tracker/Models/ApplicationDbContext.cs
@@ -1,6 +1,13 @@
-﻿namespace Expense_Tracker.Models
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Expense_Tracker.Models
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext:DbContext
     {
+        public ApplicationDbContext(DbContextOptions options):base(options)
+        {}
+
+        public DbSet<Transaction> Transactions { get; set; }
+        public DbSet<Category> Categories { get; set; }
     }
 }
